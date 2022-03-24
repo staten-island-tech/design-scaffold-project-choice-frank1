c0c6a6a73091123bcc9f54bc9c8ee974c54924bb
fz648 <90792745+fz648@users.noreply.github.com>
2022-03-24 13:58:37 -0400

    1

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,13 @@
 import "./style.css";
 import { gsap } from "gsap";
-const tl = gsap.timeline({ delay: 0.2 });
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline({ scrollTrigger: ".head7", delay: 0.1 });
 
-tl.from(".head", { opacity: 0, duration: 5 });
+tl.from(".head1", { opacity: 0, duration: 0.6, ease: "ease-out" });
+tl.from(".head2", { opacity: 0, duration: 0.6, ease: "ease-out" });
+tl.from(".head3", { opacity: 0, duration: 0.6, ease: "ease-out" });
+tl.from(".head4", { opacity: 0, duration: 0.6, ease: "ease-out" });
+tl.from(".head5", { opacity: 0, duration: 0.6, ease: "ease-out" });
+tl.from(".head6", { opacity: 0, duration: 0.6, ease: "ease-out" });
+tl.from(".head7", { opacity: 0, duration: 0.6, ease: "ease-out" });
