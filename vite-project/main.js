0d3e597ed1443020233dea141f88360dd8deb473
fz648 <90792745+fz648@users.noreply.github.com>
2022-03-28 13:57:13 -0400

    Home

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,12 +2,15 @@ import "./style.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-const tl = gsap.timeline({ scrollTrigger: ".head7", delay: 0.1 });
+const Home = gsap.timeline({ delay: 0.1 });
 
-tl.from(".head1", { opacity: 0, duration: 0.6, ease: "ease-out" });
-tl.from(".head2", { opacity: 0, duration: 0.6, ease: "ease-out" });
-tl.from(".head3", { opacity: 0, duration: 0.6, ease: "ease-out" });
-tl.from(".head4", { opacity: 0, duration: 0.6, ease: "ease-out" });
-tl.from(".head5", { opacity: 0, duration: 0.6, ease: "ease-out" });
-tl.from(".head6", { opacity: 0, duration: 0.6, ease: "ease-out" });
-tl.from(".head7", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from(".head1", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from(".head2", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from(".head3", { opacity: 0, duration: 0.6, ease: "ease-out" });
+
+ScrollTrigger.create({
+  trigger: "#header",
+  start: "top top",
+  end: "bottom 100px",
+  pin: "#header",
+});
