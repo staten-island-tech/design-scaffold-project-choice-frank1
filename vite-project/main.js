d1a667c3764dfc848437f9cdcfc27bc894d803f4
fz648 <90792745+fz648@users.noreply.github.com>
2022-03-29 13:55:36 -0400

    2

--- vite-project/main.js
+++ vite-project/main.js
@@ -7,10 +7,3 @@ const Home = gsap.timeline({ delay: 0.1 });
 Home.from(".head1", { opacity: 0, duration: 0.6, ease: "ease-out" });
 Home.from(".head2", { opacity: 0, duration: 0.6, ease: "ease-out" });
 Home.from(".head3", { opacity: 0, duration: 0.6, ease: "ease-out" });
-
-ScrollTrigger.create({
-  trigger: "#header",
-  start: "top top",
-  end: "bottom 100px",
-  pin: "#header",
-});
