0f30b6d3b81b12b2038c354448c623eef658962c
fz648 <90792745+fz648@users.noreply.github.com>
2022-03-31 13:58:12 -0400

    s

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,6 +4,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 const Home = gsap.timeline({ delay: 0.1 });
 
-Home.from(".head1", { opacity: 0, duration: 0.6, ease: "ease-out" });
-Home.from(".head2", { opacity: 0, duration: 0.6, ease: "ease-out" });
-Home.from(".head3", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from("#home1", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from("#home2", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from("#home3", { opacity: 0, duration: 0.6, ease: "ease-out" });
+Home.from(".Home-Image", { opacity: 0, duration: 0.6, ease: "ease-out" });
