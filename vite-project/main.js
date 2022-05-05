f7a4d8da0bfb120b7b67b06f91f9cb9b84e00d82
fz648 <90792745+fz648@users.noreply.github.com>
2022-05-05 13:56:23 -0400

    eff

--- vite-project/main.js
+++ vite-project/main.js
@@ -5,6 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 const DOMSelectors = {
   Destinations: document.querySelector(".Destinations"),
   Foods: document.querySelector(".Food"),
+  FoodCard: document.querySelector(".Food-Card"),
 };
 
 const Destination = [
@@ -46,28 +47,28 @@ const Food = [
   },
   {
     name: "Risotto",
-    img: "https://insanelygoodrecipes.com/wp-content/uploads/2020/03/Risotto.png",
-    desc: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency. The broth can be derived from meat, fish, or vegetables. Many types of risotto contain butter, onion, white wine, and Parmigiano-Reggiano.",
+    img: "https://www.macheesmo.com/wp-content/uploads/2020/05/Rotisserie-Chicken-Risotto-1008x700.jpg",
+    desc: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency. The broth can be derived from meat, fish, or vegetables.",
   },
   {
     name: "Polenta",
     img: "https://www.seriouseats.com/thmb/mwfbHAickLC3Av2pjGhz4uuMpjE=/1125x1125/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2015__05__20150501-polenta-vicky-wasik-5-018e7507df9940f9b1d9cd0b5f67339b.jpg",
-    desc: "Polenta is a dish of boiled cornmeal that was historically made from other grains. The dish comes from Italy. It may be served as a hot porridge, or it may be allowed to cool and solidify into a loaf that can be baked, fried, or grilled.",
+    desc: "Polenta is a dish of boiled cornmeal. It may be served as a hot porridge, or it may be allowed to cool and solidify into a loaf that can be baked, fried, or grilled.",
   },
   {
-    name: "Colosseum",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMspD7Duc-yuSto9h4Rb_Pum6YUbyiPCvFKQ&usqp=CAU",
-    desc: "The colosseum is",
+    name: "Lasagne",
+    img: "https://img.taste.com.au/N5uOwvqw/taste/2021/03/sweet-potato-and-lentil-lasagne-170423-2.jpg",
+    desc: "Lasagne is really good",
   },
   {
-    name: "Leaning Tower Of Pisa",
-    img: "https://www.nuestro.cl/wp-content/uploads/2021/10/Tower-of-Pisa.jpg",
-    desc: "The colosseum is",
+    name: "Carbonara",
+    img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/0346a29a89ef229b1a0ff9697184f944/Derivates/cb5051204f4a4525c8b013c16418ae2904e737b7.jpg",
+    desc: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency. The broth can be derived from meat, fish, or vegetables.",
   },
   {
-    name: "Sample",
-    img: "https://www.nuestro.cl/wp-content/uploads/2021/10/Tower-of-Pisa.jpg",
-    desc: "The colosseum is",
+    name: "Penne",
+    img: "https://afoodloverskitchen.com/wp-content/uploads/2021/04/penne-al-forno-feat.jpg",
+    desc: "Polenta is a dish of boiled cornmeal. It may be served as a hot porridge, or it may be allowed to cool and solidify into a loaf that can be baked, fried, or grilled.",
   },
 ];
 function DisplayFoods() {
@@ -88,9 +89,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Home = gsap.timeline({ delay: 0.1 });
 Home.from(".Home", { opacity: 0, duration: 0.6, ease: "ease-out" });
-Home.from("#home1", { opacity: 0, rotation: 720, duration: 0.8 });
-Home.from("#home2", { opacity: 0, rotation: 720, duration: 0.8 });
-Home.from("#home3", { opacity: 0, rotation: 720, duration: 0.8 });
+Home.from("#home1", { opacity: 0, rotation: 180, duration: 0.8 });
+Home.from("#home2", { opacity: 0, rotation: 180, duration: 0.8 });
+Home.from("#home3", { opacity: 0, rotation: 180, duration: 0.8 });
 
 const DestinationAnimation = gsap.timeline({
   delay: 0.8,
@@ -98,22 +99,23 @@ const DestinationAnimation = gsap.timeline({
 });
 DestinationAnimation.from(".Destinations-Head", {
   opacity: 0,
-  rotation: 90,
   duration: 1.5,
 });
 DestinationAnimation.from(".Destinations-Card", {
   opacity: 0,
-  rotation: 10,
   duration: 1.5,
 });
 DestinationAnimation.from(".Destinations-Image", {
   duration: 4,
-  clipPath: "Polygon( 0% 0%, 100% 0%, 100% 100%, 0% 100%)",
   opacity: 0,
 });
 
 const FoodAnimation = gsap.timeline({
   delay: 0.8,
-  scrollTrigger: "Destinations-Head",
+  scrollTrigger: "Food-Head",
 });
-FoodAnimation.from(".Food-Head", {});
+FoodAnimation.from(".Food-Head", {
+  duration: 2,
+  opacity: 100,
+  repeat: 100,
+});
