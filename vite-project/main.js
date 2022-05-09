c0c6b693185c68b5a2d15c88592e75460236bb69
fz648 <90792745+fz648@users.noreply.github.com>
2022-05-09 13:57:38 -0400

    alts and styling

--- vite-project/main.js
+++ vite-project/main.js
@@ -12,17 +12,20 @@ const Destination = [
   {
     name: "Leaning Tower Of Pisa",
     img: "https://www.nuestro.cl/wp-content/uploads/2021/10/Tower-of-Pisa.jpg",
-    desc: "The Leaning Tower of Pisa is",
+    desc: "The Leaning Tower of Pisa is pretty lean",
+    alt: "The leaning tower of pisa in the sunlight",
   },
   {
     name: "Pantheon",
     img: "https://www.planetware.com/photos-large/I/italy-rome-pantheon-exterior-view.jpg",
     desc: "The Pantheon is",
+    alt: "The front of the Pantheon",
   },
   {
     name: "Colosseum",
     img: "https://media.nationalgeographic.org/assets/photos/198/897/95a24eaa-5e33-4778-b06c-96654f3dbf87.jpg",
     desc: "The colosseum is",
+    alt: "The colosseum under the clouds",
   },
 ];
 function DisplayDestinations() {
@@ -30,7 +33,7 @@ function DisplayDestinations() {
     DOMSelectors.Destinations.insertAdjacentHTML(
       "afterbegin",
       `<div class="Destinations-Card">
-      <img id="Destinations-Image" src="${Destination.img}">
+      <img alt="${Destination.alt}" id="Destinations-Image" src="${Destination.img}">
       <h5 class="Destinations-Card-Title">${Destination.name}</h5>
       <p class="Destinations-Desc">${Destination.desc}</p>
    </div>`
@@ -44,31 +47,37 @@ const Food = [
     name: "Pizza",
     img: "https://www.eataly.com/wp/wp-content/uploads/2018/08/ech-quattro-mani-matt-roan-pizza-horizontal-web.jpg",
     desc: "Pizza is really good",
+    alt: "Delicious Pizza",
   },
   {
     name: "Risotto",
     img: "https://www.macheesmo.com/wp-content/uploads/2020/05/Rotisserie-Chicken-Risotto-1008x700.jpg",
     desc: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency. The broth can be derived from meat, fish, or vegetables.",
+    alt: "Rice",
   },
   {
     name: "Polenta",
     img: "https://www.seriouseats.com/thmb/mwfbHAickLC3Av2pjGhz4uuMpjE=/1125x1125/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2015__05__20150501-polenta-vicky-wasik-5-018e7507df9940f9b1d9cd0b5f67339b.jpg",
     desc: "Polenta is a dish of boiled cornmeal. It may be served as a hot porridge, or it may be allowed to cool and solidify into a loaf that can be baked, fried, or grilled.",
+    alt: "Polenta",
   },
   {
     name: "Lasagne",
     img: "https://img.taste.com.au/N5uOwvqw/taste/2021/03/sweet-potato-and-lentil-lasagne-170423-2.jpg",
     desc: "Lasagne is really good",
+    alt: "A cube of Lasgne",
   },
   {
     name: "Carbonara",
     img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/0346a29a89ef229b1a0ff9697184f944/Derivates/cb5051204f4a4525c8b013c16418ae2904e737b7.jpg",
     desc: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency. The broth can be derived from meat, fish, or vegetables.",
+    alt: "Carbonara in the shape of a hill",
   },
   {
     name: "Penne",
     img: "https://afoodloverskitchen.com/wp-content/uploads/2021/04/penne-al-forno-feat.jpg",
     desc: "Polenta is a dish of boiled cornmeal. It may be served as a hot porridge, or it may be allowed to cool and solidify into a loaf that can be baked, fried, or grilled.",
+    alt: "Tasty Penne",
   },
 ];
 function DisplayFoods() {
@@ -76,7 +85,7 @@ function DisplayFoods() {
     DOMSelectors.Foods.insertAdjacentHTML(
       "beforeend",
       `<div class="Food-Card">
-      <img id="Food-Image" src="${Food.img}">
+      <img id="Food-Image" alt="${Food.alt}" src="${Food.img}">
       <h5 class="Food-Card-Title">${Food.name}</h5>
       <p class="Food-Desc">${Food.desc}</p>
    </div>`
@@ -115,7 +124,14 @@ const FoodAnimation = gsap.timeline({
   scrollTrigger: "Food-Head",
 });
 FoodAnimation.from(".Food-Head", {
-  duration: 2,
-  opacity: 100,
-  repeat: 100,
+  duration: 1,
+  opacity: 0,
+});
+FoodAnimation.from(".Food", {
+  duration: 1,
+  opacity: 0,
+});
+FoodAnimation.from(".Food-Card", {
+  duration: 1,
+  opacity: 0,
 });
